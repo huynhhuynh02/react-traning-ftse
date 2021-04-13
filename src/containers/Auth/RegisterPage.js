3965df10d877869446a2f58794025b4dbf9fc88f
MaiHuynhLoc <mhloc1998@gmail.com>
2021-04-13 19:22:02 +0700

    RegisterPage + HomePage

--- src/containers/Auth/RegisterPage.js
+++ src/containers/Auth/RegisterPage.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {Jumbotron, Button} from 'react-bootstrap';
+import { Button} from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import "../../styles/RegisterPage.css";
 class RegisterPage extends React.Component
 {
     constructor(props){
@@ -11,9 +15,35 @@ class RegisterPage extends React.Component
     render(){
         return(
             <>
-                <Jumbotron>
-                    <h1>Register Page</h1>
-                </Jumbotron>
+                <div className="wrapper">
+                <Container>
+                    <Row >
+                        <Col md={{offset: 3, span: 6}}>
+                        <div class="back">
+                            <svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M9.8627 2.225L8.37936 0.75L0.137695 9L8.3877 17.25L9.8627 15.775L3.0877 9L9.8627 2.225Z" fill="#8692A6"/>
+                            </svg>
+                        
+                        <a id="back" href="/">Back</a>
+                        </div>
+                            <div class="form">
+                                <p id="register-title">Register Indidual Account!</p>
+                                        <p id="description">For the purpose of chatters regulation, your details are required.</p>
+                                            <form class="register-form">
+                                                <input type="email" placeholder="Email" />
+                                                    <input type="password" placeholder="Password" />
+                                                        <input type="password" placeholder="Reset password" />
+                                                            <label class="md-radio">I agree to terms & conditions
+                                                                <input type="radio" name="radio" id="1" />
+                                                                    <span class="checkmark"></span>
+                                                                        </label>
+                                                                            <button>Register</button>
+                                            </form>
+                            </div>
+                    </Col>
+                    </Row>
+                </Container>
+                </div>    
             </>
             
         );
--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,20 +1,231 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {Jumbotron, Button} from 'react-bootstrap';
+import { Button} from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import "../styles/HomePage.css";
+import 'font-awesome/css/font-awesome.min.css';
 class HomePage extends React.Component
 {
     constructor(props){
         super(props);
+        this.toggleClass = this.toggleClass.bind(this);
+        this.expandClass = this.expandClass.bind(this);
+
+        this.state = {
+            active: false,
+            isExpanded: false,
+            status: ""
+        };
         
+    }
+    toggleClass() {
+        const currentState = this.state.active;
+        this.setState({ active: !currentState });
+    }
+    expandClass() {
+        const currentExpanded = this.state.isExpanded;
+        this.setState({ isExpanded: !currentExpanded });
     }
         
     render(){
         return(
-            <>
-                <Jumbotron>
-                    <h1>Home Page</h1>
-                   
-                </Jumbotron>
+    <>
+    <div id="frame">
+	<div id="sidepanel">
+		<div id="profile" class={this.state.isExpanded ? 'expanded' : null}>
+			<div class="wrap">
+				<img id="profile-img" src="img/myavatar.jpg" class={this.state.status} alt="" onClick={this.toggleClass}/>
+				<p>Mai Huynh Loc</p>
+				<i class="fa fa-chevron-down expand-button" aria-hidden="true" onClick={this.expandClass}></i>
+				<div id="status-options" class={this.state.active ? 'active' : null}>
+					<ul>
+						<li id="status-online" onClick={()=>{this.setState({status:'online'})}}><span class="status-circle"></span> <p>Online</p></li>
+						<li id="status-away" onClick={()=>{this.setState({status:'away'})}}><span class="status-circle" ></span> <p>Away</p></li>
+						<li id="status-busy" onClick={()=>{this.setState({status:'busy'})}}><span class="status-circle" ></span> <p>Busy</p></li>
+						<li id="status-offline" onClick={()=>{this.setState({status:'offline'})}}><span class="status-circle" ></span> <p>Offline</p></li>
+					</ul>
+				</div>
+				<div id="expanded">
+					<label for="facebook"><i class="fa fa-facebook fa-fw" aria-hidden="true"></i></label>
+					<input name="facebook" type="text" value="maihuynhloc" />
+					<label for="twitter"><i class="fa fa-twitter fa-fw" aria-hidden="true"></i></label>
+					<input name="twitter" type="text" value="loc98" />
+					<label for="instagram"><i class="fa fa-instagram fa-fw" aria-hidden="true"></i></label>
+					<input name="instagram" type="text" value="mhloc98" />
+				</div>
+			</div>
+		</div>
+		<div id="search">
+			<label for=""><i class="fa fa-search" aria-hidden="true"></i></label>
+			<input type="text" placeholder="Search contacts..." />
+		</div>
+		<div id="contacts" class={this.state.isExpanded ? 'expanded' : null}>
+			<ul>
+				<li class="contact">
+					<div class="wrap">
+						<span class="contact-status online"></span>
+						<img src="http://emilcarlsson.se/assets/louislitt.png" alt="" />
+						<div class="meta">
+							<p class="name">Louis Litt</p>
+							<p class="preview">You just got LITT up, Mike.</p>
+						</div>
+					</div>
+				</li>
+				<li class="contact active">
+					<div class="wrap">
+						<span class="contact-status busy"></span>
+						<img src="http://emilcarlsson.se/assets/harveyspecter.png" alt="" />
+						<div class="meta">
+							<p class="name">Harvey Specter</p>
+							<p class="preview">Wrong. You take the gun, or you pull out a bigger one. Or, you call their bluff. Or, you do any one of a hundred and forty six other things.</p>
+						</div>
+					</div>
+				</li>
+				<li class="contact">
+					<div class="wrap">
+						<span class="contact-status away"></span>
+						<img src="http://emilcarlsson.se/assets/rachelzane.png" alt="" />
+						<div class="meta">
+							<p class="name">Rachel Zane</p>
+							<p class="preview">I was thinking that we could have chicken tonight, sounds good?</p>
+						</div>
+					</div>
+				</li>
+				<li class="contact">
+					<div class="wrap">
+						<span class="contact-status online"></span>
+						<img src="http://emilcarlsson.se/assets/donnapaulsen.png" alt="" />
+						<div class="meta">
+							<p class="name">Donna Paulsen</p>
+							<p class="preview">Mike, I know everything! I'm Donna..</p>
+						</div>
+					</div>
+				</li>
+				<li class="contact">
+					<div class="wrap">
+						<span class="contact-status busy"></span>
+						<img src="http://emilcarlsson.se/assets/jessicapearson.png" alt="" />
+						<div class="meta">
+							<p class="name">Jessica Pearson</p>
+							<p class="preview">Have you finished the draft on the Hinsenburg deal?</p>
+						</div>
+					</div>
+				</li>
+				<li class="contact">
+					<div class="wrap">
+						<span class="contact-status"></span>
+						<img src="http://emilcarlsson.se/assets/haroldgunderson.png" alt="" />
+						<div class="meta">
+							<p class="name">Harold Gunderson</p>
+							<p class="preview">Thanks Mike! :)</p>
+						</div>
+					</div>
+				</li>
+				<li class="contact">
+					<div class="wrap">
+						<span class="contact-status"></span>
+						<img src="http://emilcarlsson.se/assets/danielhardman.png" alt="" />
+						<div class="meta">
+							<p class="name">Daniel Hardman</p>
+							<p class="preview">We'll meet again, Mike. Tell Jessica I said 'Hi'.</p>
+						</div>
+					</div>
+				</li>
+				<li class="contact">
+					<div class="wrap">
+						<span class="contact-status busy"></span>
+						<img src="http://emilcarlsson.se/assets/katrinabennett.png" alt="" />
+						<div class="meta">
+							<p class="name">Katrina Bennett</p>
+							<p class="preview">I've sent you the files for the Garrett trial.</p>
+						</div>
+					</div>
+				</li>
+				<li class="contact">
+					<div class="wrap">
+						<span class="contact-status"></span>
+						<img src="http://emilcarlsson.se/assets/charlesforstman.png" alt="" />
+						<div class="meta">
+							<p class="name">Charles Forstman</p>
+							<p class="preview">Mike, this isn't over.</p>
+						</div>
+					</div>
+				</li>
+				<li class="contact">
+					<div class="wrap">
+						<span class="contact-status"></span>
+						<img src="http://emilcarlsson.se/assets/jonathansidwell.png" alt="" />
+						<div class="meta">
+							<p class="name">Jonathan Sidwell</p>
+							<p class="preview"><span>You:</span> That's bullshit. This deal is solid.</p>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+		<div id="bottom-bar">
+			<button id="addcontact"><i class="fa fa-user-plus fa-fw" aria-hidden="true"></i> <span>Add contact</span></button>
+			<button id="settings"><i class="fa fa-cog fa-fw" aria-hidden="true"></i> <span>Settings</span></button>
+		</div>
+	</div>
+	<div class="content">
+		<div class="contact-profile">
+			<img src="http://emilcarlsson.se/assets/harveyspecter.png" alt="" />
+			<p>Harvey Specter</p>
+			<div class="social-media">
+				<i class="fa fa-facebook" aria-hidden="true"></i>
+				<i class="fa fa-twitter" aria-hidden="true"></i>
+				 <i class="fa fa-instagram" aria-hidden="true"></i>
+			</div>
+		</div>
+		<div class="messages">
+			<ul>
+				<li class="sent">
+					<img src="img/myavatar.jpg" alt="" />
+					<p>How the hell am I supposed to get a jury to believe you when I am not even sure that I do?!</p>
+				</li>
+				<li class="replies">
+					<img src="http://emilcarlsson.se/assets/harveyspecter.png" alt="" />
+					<p>When you're backed against the wall, break the god damn thing down.</p>
+				</li>
+				<li class="replies">
+					<img src="http://emilcarlsson.se/assets/harveyspecter.png" alt="" />
+					<p>Excuses don't win championships.</p>
+				</li>
+				<li class="sent">
+					<img src="/img/myavatar.jpg" alt="" />
+					<p>Oh yeah, did Michael Jordan tell you that?</p>
+				</li>
+				<li class="replies">
+					<img src="http://emilcarlsson.se/assets/harveyspecter.png" alt="" />
+					<p>No, I told him that.</p>
+				</li>
+				<li class="replies">
+					<img src="http://emilcarlsson.se/assets/harveyspecter.png" alt="" />
+					<p>What are your choices when someone puts a gun to your head?</p>
+				</li>
+				<li class="sent">
+					<img src="/img/myavatar.jpg" alt="" />
+					<p>What are you talking about? You do what they say or they shoot you.</p>
+				</li>
+				<li class="replies">
+					<img src="http://emilcarlsson.se/assets/harveyspecter.png" alt="" />
+					<p>Wrong. You take the gun, or you pull out a bigger one. Or, you call their bluff. Or, you do any one of a hundred and forty six other things.</p>
+				</li>
+			</ul>
+		</div>
+		<div class="message-input">
+			<div class="wrap">
+			<input type="text" placeholder="Write your message..." />
+			<i class="fa fa-paperclip attachment" aria-hidden="true"></i>
+			<button class="submit"><i class="fa fa-paper-plane" aria-hidden="true"></i></button>
+			</div>
+		</div>
+	</div>
+</div>
+
             </>
             
         );
